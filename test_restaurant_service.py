86c4f981c9148180406ff45be74795612b1e558f
Timur <wellychar@gmail.com>
2024-03-19 15:51:06 +0300

    3

---
+++ test_restaurant_service.py
@@ -0,0 +1,49 @@
+import pytest
+from app.api.models import RestaurantIn, RestaurantOut
+
+restaurant = RestaurantIn(
+    name='Blackstar',
+    addres='Moscow',
+    menu='beef',
+    phone='+7123456789'
+)
+
+
+def test_create_restaurant(restaurant: RestaurantIn = restaurant):
+    assert dict(restaurant) == {'name': restaurant.name,
+                              'address': restaurant.address,
+                              'menu': restaurant.menu,
+                              'phone': restaurant.phone
+                              }
+
+
+def test_update_restaurant_menu(restaurant: RestaurantIn = restaurant):
+    restaurant_upd = RestaurantOut(
+        name='Blackstar',
+        addres='Moscow',
+        menu='beef',
+        phone='+7123456789',
+        id=1
+    )
+    assert dict(restaurant_upd) == {'name': restaurant.name,
+                              'address': restaurant.address,
+                              'menu': restaurant.menu,
+                              'phone': restaurant.phone,
+                              'id': restaurant_upd.id
+                              }
+
+
+def test_update_restaurant_genre(restaurant: RestaurantIn = restaurant):
+    restaurant_upd = RestaurantOut(
+        name=restaurant.name,
+        address=restaurant.address,
+        city=restaurant.city,
+        phone = restaurant.phone,
+        id=1
+    )
+    assert dict(restaurant_upd) == {'name': restaurant.name,
+                              'address': restaurant.address,
+                              'city': restaurant.city,
+                              'phone': restaurant.phone,
+                              'id': restaurant_upd.id
+                              }
